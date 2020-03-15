c7d9704d93e56256364ddf02a6cf680f30468403
Suzy Paeta <suzy.paeta@gmail.com>
2020-03-15 12:35:14 +0100

    Flutter web

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.grey[200],
+          body: new Stack(
+            children: <Widget>[
+              new Container(
+                decoration: new BoxDecoration(
+                  image: new DecorationImage(
+                    image: new AssetImage("images/DSC_8940.jpg"),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              new Center(
+                child: new Text("Hello background"),
+              )
+            ],
+          )),
+    );
+  }
+}
